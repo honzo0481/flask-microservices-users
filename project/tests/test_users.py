5337fe8c56cc029710cbdbffaa85be41f4b89df3
Robert Gonzales <gonzalesre@gmail.com>
2017-06-23 14:00:04 -0700

    added a create user test.

--- project/tests/test_users.py
+++ project/tests/test_users.py
@@ -14,3 +14,19 @@ class TestUserService(BaseTestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn('pong!', data['message'])
         self.assertIn('success', data['status'])
+
+    def test_add_user(self):
+        """Adding a new user to the db should be possible."""
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=json.dumps(dict(
+                    useruser='robert',
+                    email='gonzalesre@gmail.com'
+                )),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            self.assertIn('gonzalesre@gmail.com was added!', data['message'])
+            self.assertIn('success', data['status'])
