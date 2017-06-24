d181915be81cd0d27ea7a3459b4d41b37a3ba802
honzo0481 <gonzalesre@gmail.com>
2017-06-24 00:22:12 -0700

    added tests for empty json, invalid keys, and duplicate user.

--- project/tests/test_users.py
+++ project/tests/test_users.py
@@ -30,3 +30,55 @@ class TestUserService(BaseTestCase):
             self.assertEqual(response.status_code, 201)
             self.assertIn('gonzalesre@gmail.com was added!', data['message'])
             self.assertIn('success', data['status'])
+
+    def test_add_user_invalid_json(self):
+        """An error should be thrown if the JSON object is empty."""
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=json.dumps({}),
+                content_type='application/json'
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Invalid payload.', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_add_user_invalid_json_keys(self):
+        """An error should be thrown if the JSON object does not have a username key."""
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=json.dumps(dict(email='gonzalesre@gmail.com')),
+                content_type='application/json'
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Invalid payload.', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_add_user_duplicate_user(self):
+        """An error should be thrown if the email already exists."""
+        with self.client:
+            self.client.post(
+                '/users',
+                data=json.dumps(dict(
+                    username='rob',
+                    email='gonzalesre@gmail.com'
+                )),
+                content_type='application/json'
+            )
+            response = self.client.post(
+                '/users',
+                data=json.dumps(dict(
+                    username='rob',
+                    email='gonzalesre@gmail.com'
+                )),
+                content_type='application/json'
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn(
+                'Sorry. That email already exists.', data['message']
+            )
+            self.assertIn('fail', data['status'])
