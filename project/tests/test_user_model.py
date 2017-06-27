97c93dbce4f8849e22d55a44814f8e661a888bf7
honzo0481 <gonzalesre@gmail.com>
2017-06-26 20:58:06 -0700

    added tests for duplicate users.

--- project/tests/test_user_model.py
+++ project/tests/test_user_model.py
@@ -3,6 +3,7 @@
 from project import db
 from project.api.models import User
 from project.api.tests.base import BaseTestCase
+from sqlalchemy.exc import IntegrityError
 
 
 class TestUserModel(BaseTestCase):
@@ -21,3 +22,33 @@ class TestUserModel(BaseTestCase):
         self.assertEqual(user.email, 'test@test.com')
         self.assertTrue(user.active)
         self.assertTrue(user.created_at)
+
+    def test_add_user_duplicate_username(self):
+        """Adding a duplicate username should fail."""
+        user = User(
+            username='test@test.com',
+            email='test@test.com'
+        )
+        db.session.add(user)
+        db.session.commit()
+        duplicate_user = User(
+            username='test@test.com',
+            email='test@test2.com'
+        )
+        db.session.add(duplicate_user)
+        self.assertRaises(IntegrityError, db.session.commit)
+
+    def test_add_user_duplicate_email(self):
+        """Adding a duplicate email should fail."""
+        user = User(
+            username='test@test.com',
+            email='test@test.com'
+        )
+        db.session.add(user)
+        db.session.commit()
+        duplicate_user = User(
+            username='test@test2.com',
+            email='test@test.com'
+        )
+        db.session.add(duplicate_user)
+        self.assertRaises(IntegrityError, db.session.commit)
